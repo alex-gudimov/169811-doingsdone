d0ab1e5938eda4fe85be0e93140a007e048c66fe
alex-gudimov <gudimovalexey@gmail.com>
2018-11-26 22:34:54 +0300

    Внес доработки в файл schema.sql; обновил базу doingsdone

--- schema.sql
+++ schema.sql
@@ -14,10 +14,10 @@ CREATE TABLE `projects` (
 
 CREATE TABLE `users` (
 	`id`					int(11) unsigned NOT NULL AUTO_INCREMENT,
-	`dt_add`			timestamp default current_timestamp NOT NULL,
+	`created_at`		timestamp default current_timestamp NOT NULL,
 	`email`				char(255) NOT NULL,
 	`user_name`	char(255) NOT NULL,
-	`password` 		char(64),
+	`password` 		char(255),
 	PRIMARY KEY (`id`),
 	UNIQUE KEY `email` (`email`),
 	UNIQUE KEY `user_name` (`user_name`)
@@ -25,19 +25,21 @@ CREATE TABLE `users` (
 
 CREATE TABLE `tasks` (
 	`id`					int(11) unsigned NOT NULL AUTO_INCREMENT,
-	`dt_add`			timestamp default current_timestamp NOT NULL,
-	`dt_comp`		timestamp NOT NULL,
-	`status`			char(64) default 0,
+	`created_at`		timestamp default current_timestamp NOT NULL,
+	`completed_at`	timestamp NOT NULL,
+	`status`			tinyint(2) default 0,
 	`project_id`		int(11),
 	`user_id`			int(11),
 	`task_name`		char(255) NOT NULL,
 	`file_add` 		char(255),
-	`dt_deadline`	timestamp,
-	PRIMARY KEY (`id`),
-	UNIQUE KEY `task_name` (`task_name`)
+	`deadline`		timestamp,
+	PRIMARY KEY (`id`)
 );
 
 CREATE UNIQUE INDEX project_name_ind ON projects(project_name);
 CREATE UNIQUE INDEX email_ind ON users(email);
 CREATE UNIQUE INDEX user_name_ind ON users(user_name);
 CREATE UNIQUE INDEX task_name_ind ON tasks(task_name);
+
+CREATE INDEX user_id_ind ON tasks(user_id);
+CREATE INDEX project_id_ind ON tasks(project_id);
